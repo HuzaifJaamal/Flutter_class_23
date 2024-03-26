ea9bece3a984cef58b0158b7467af97207af45f2
HuzaifaJamal <huzaifajamal95@gmail.com>
2024-03-26 13:23:15 +0500

    Cart and Add Favorite Functionality

--- lib/data/product_list.dart
+++ lib/data/product_list.dart
@@ -2,13 +2,16 @@ List productData = [
   {
     "name":"Biryani",
     "price":400,
+    "isFav": false,
   },
   {
     "name":"Burger",
     "price":700,
+    "isFav": false,
   },
   {
     "name":"Karahi",
     "price":1000,
+    "isFav": false,
   }
 ];
--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -1,7 +1,6 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_class_23/data/product_list.dart';
+import 'package:flutter_class_23/screen/cart_view.dart';
 
 class HomeView extends StatefulWidget {
   const HomeView({super.key});
@@ -12,6 +11,8 @@ class HomeView extends StatefulWidget {
 
 class _HomeViewState extends State<HomeView> {
 List cartItems = [];
+List favouriItems = [];
+
 
   @override
   Widget build(BuildContext context) {
@@ -24,25 +25,58 @@ List cartItems = [];
         itemBuilder: (context,index){
           return Container(
             margin: const EdgeInsets.only(bottom: 5),
-            child: ListTile(
-              tileColor: Colors.grey,
-              title: Text(productData[index]["name"]),
-              subtitle: Text(productData[index]["price"].toString()),
-              trailing: IconButton(
-                onPressed: () {
-                  setState(() {
-                    
-                  cartItems.add(productData[index]);
-                  });
-                },
-                icon: const Icon(Icons.add),
-              ),
+            child: Column(
+              children: [
+                ListTile(
+                  tileColor: Colors.grey,
+                  title: Text(productData[index]["name"]),
+                  subtitle: Text(productData[index]["price"].toString()),
+                  trailing: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      IconButton(
+                        onPressed: () {
+                          setState(() {
+                            
+                          cartItems.add(productData[index]);
+                          });
+                        },
+                        icon: const Icon(Icons.add),
+                      ),
+                      IconButton(
+                        onPressed: () {
+                          setState(() {
+                            if(productData[index]["isFav"]== true){
+                              productData[index]["isFav"] = false;
+                              favouriItems.remove(productData[index]);
+                            }else{
+                              productData[index]["isFav"] = true;
+                              favouriItems.add(productData[index]);
+                            }
+                          });
+                        },
+                        icon: const Icon(Icons.favorite),
+                        color: productData[index]["isFav"]
+                        ?Colors.red
+                        :Colors.white,
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             ),
           );
         },
         ),
         floatingActionButton: FloatingActionButton(
-          onPressed: (){},
+          onPressed: () async{
+          await Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context)=> CartView(
+                items: cartItems,
+              )));
+              setState(() {});
+          },
           child: const Icon(Icons.home),
           ),
     );
